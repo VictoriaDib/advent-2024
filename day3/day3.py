c8a68e19ce0ec873dfdc6d2609a2ed6a552671d2
Victoria Dib <vdib@buffalo.edu>
2024-12-04 05:36:03 -0500

    day 3 pt 2 done

--- day3/day3.py
+++ day3/day3.py
@@ -1,5 +1,6 @@
 import re
 
+# part 1
 input = ""
 with open('input.txt') as f:
     input = f.read()
@@ -11,4 +12,21 @@ total = 0
 for pair in matches:
     total += int(pair[0]) * int(pair[1])
 
-print(total)
+print("Pt 1: ", total)
+
+# part 2
+total = 0
+matches = re.findall(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)|(do\(\)|don\'t\(\))", input)
+cond = False
+for pair in matches:
+    if pair[2] == "don't()":
+        cond = True
+        continue
+    elif pair[2] == "do()":
+        cond = False
+        continue
+    if cond is False:
+        total += int(pair[0]) * int(pair[1])
+
+print("pt 2: ", total)  
+
